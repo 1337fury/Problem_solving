67bd26ddd0366974660f498f9d479f2f10da3cba
1337fury <elouardiabdelhalim@gmail.com>
2023-08-22 10:19:11 +0100

    Reverse Linked List [√]

---
+++ Easy/ReverseLinkedList.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// Definition for singly-linked list.
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *curr;
+        ListNode *prev;
+        ListNode *nxt;
+
+        curr = head;
+        prev = NULL;
+        while (curr)
+        {
+            nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+};
