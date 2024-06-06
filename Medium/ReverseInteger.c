5f5b6cf145b3057668cc464e99f331b7e267bad5
1337fury <elouardyabdelhalim@gmail.com>
2024-06-06 16:45:45 +0100

    Add implementation of reverse integer algorithm in C

---
+++ Medium/ReverseInteger.c
@@ -0,0 +1,13 @@
+int reverse(int x){
+    long rs = 0;
+    int ld = 0;
+    while (x) {
+        ld = x % 10;
+        rs = rs * 10;
+        rs = rs + ld;
+        if (rs > INT_MAX || rs < INT_MIN)
+            return 0;
+        x /= 10;
+    }
+    return rs;
+}
